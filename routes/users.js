80169a6e0aab80934a1fae53000afc5ad4120f04
manikandanthangam <manikandantt91@gmail.com>
2019-12-26 00:25:23 +0530

    router format changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const userController = require('../controllers/user.controller')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource 1');
-});
+router.get('/', userController.getUser);
 
 module.exports = router;
